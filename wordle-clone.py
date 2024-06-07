25bc547dd5f213188ca9ee3a505e569ee6787750
nemerson <nickaemerson@gmail.com>
2024-06-07 14:08:08 -0500

    Correct color
    Changed the text color for "Correct!" to white so it stands out for terminal windows with text colors other than white

--- wordle-clone.py
+++ wordle-clone.py
@@ -5,6 +5,7 @@ todays_word = todays_word.upper()
 checked_guess = [''] * 5
 counter = 0
 
+WHITE = '\033[97m'
 BLUE = '\033[94m'
 YELLOW = '\033[93m'
 RED = '\033[91m'
@@ -13,7 +14,7 @@ RESET = '\033[0m'
 while counter < 6 :
     player_guess = input('Enter a 5 letter word: ').upper()
     if player_guess == todays_word :
-        print('Correct!')
+        print(WHITE + 'Correct!' + RESET)
         counter = 7
     else : 
         for i in range(5) :
