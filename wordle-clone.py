4818e9226b2bb65fc60d9dea403cc39b8fc40f01
nemerson <nickaemerson@gmail.com>
2024-06-07 13:37:46 -0500

    While loop
    Added while loop and a counter to limit the number of attempts to 6

--- wordle-clone.py
+++ wordle-clone.py
@@ -1,27 +1,34 @@
 todays_word = 'media'
-player_guess = input('Enter a 5 letter word: ')
+
 todays_word = todays_word.upper()
-player_guess = player_guess.upper()
+#player_guess = player_guess.upper()
 checked_guess = [''] * 5
+counter = 0
 
 GREEN = '\033[92m'
 YELLOW = '\033[93m'
 RED = '\033[91m'
 RESET = '\033[0m'
 
-if player_guess == todays_word :
-    print('Correct!')
-else : 
-    for i in range(5) :
-        if player_guess[i] == todays_word[i] :
-            checked_guess[i] = GREEN + player_guess[i] + RESET
-        elif player_guess[i] in todays_word :
-            checked_guess[i] = YELLOW + player_guess[i] + RESET
-        else :
-            checked_guess[i] = RED + player_guess[i] + RESET
-    checked_guess_string = ''.join(checked_guess)
-    print(checked_guess_string)
-
+while counter < 6 :
+    player_guess = input('Enter a 5 letter word: ').upper()
+    if player_guess == todays_word :
+        print('Correct!')
+        counter = 7
+    else : 
+        for i in range(5) :
+            if player_guess[i] == todays_word[i] :
+                checked_guess[i] = GREEN + player_guess[i] + RESET
+            elif player_guess[i] in todays_word :
+                checked_guess[i] = YELLOW + player_guess[i] + RESET
+            else :
+                checked_guess[i] = RED + player_guess[i] + RESET
+        checked_guess_string = ''.join(checked_guess)
+        print(checked_guess_string)
+    counter += 1    
+    if counter == 6 :
+        print('Sorry. Try again tomorrow.')
+    
 
 #if 
 #   print('Your first letter is correct!')
