fad95e8b491bf5622017f1d479f773beb42c7988
nemerson <nickaemerson@gmail.com>
2024-06-07 12:43:15 -0500

    Output update
    Working to have output be the guessed word with Capital letter to indicate the guessed letter is in the right spot. Lower case letter to indicate letter is in today's word and an x to indicate the ltter is not in today's word.

--- wordle-clone.py
+++ wordle-clone.py
@@ -2,29 +2,35 @@ todays_word = 'media'
 player_guess = input('Enter a 5 letter word: ')
 todays_word = todays_word.lower()
 player_guess = player_guess.lower()
+checked_guess = ''
 
 
 if player_guess == todays_word :
     print('Correct!')
-else :
-    print('Sorry. Try again.')
-
-if player_guess[0] == todays_word[0] :
-    print('Your first letter is correct!')
+elif player_guess[0] == todays_word[0] :
+    checked_guess[0] = player_guess[0].upper()
 elif player_guess[0] in todays_word :
-    print('Your first letter is in today\'s word.')
+    checked_guess[0] = player_guess[0]
+else :
+    checked_guess[0] = 'x'      
 
-if player_guess[1] == todays_word[1] :
-    print('Your second letter is correct!')
-elif player_guess[1] in todays_word[1] :
-    print('Your second letter is in today\'s word.')
 
-if player_guess[2] in todays_word :
-    print('Your third letter is in today\'s word.')
+#if 
+#   print('Your first letter is correct!')
+#elif player_guess[0] in todays_word :
+#    print('Your first letter is in today\'s word.')
 
-if player_guess[3] in todays_word :
-    print('Your fourth letter is in today\'s word.')
+#if player_guess[1] == todays_word[1] :
+#    print('Your second letter is correct!')
+#elif player_guess[1] in todays_word[1] :
+#   print('Your second letter is in today\'s word.')
 
-if player_guess[4] in todays_word :
-    print('Your fifth letter is in today\'s word.')
+#if player_guess[2] in todays_word :
+#    print('Your third letter is in today\'s word.')
+
+#if player_guess[3] in todays_word :
+#    print('Your fourth letter is in today\'s word.')
+
+#if player_guess[4] in todays_word :
+#    print('Your fifth letter is in today\'s word.')
           
