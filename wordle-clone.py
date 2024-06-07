f759d96f273102add7cc489304549e954d2b63ab
nemerson <nickaemerson@gmail.com>
2024-06-07 13:10:13 -0500

    Wordle output update
    Utilized loop to simplify code for checking letters and determing output.
    Added .join to print string list as single word then added color to indicate letter statuses.

--- wordle-clone.py
+++ wordle-clone.py
@@ -1,18 +1,26 @@
 todays_word = 'media'
 player_guess = input('Enter a 5 letter word: ')
-todays_word = todays_word.lower()
-player_guess = player_guess.lower()
-checked_guess = ''
+todays_word = todays_word.upper()
+player_guess = player_guess.upper()
+checked_guess = [''] * 5
 
+GREEN = '\033[92m'
+YELLOW = '\033[93m'
+RED = '\033[91m'
+RESET = '\033[0m'
 
 if player_guess == todays_word :
     print('Correct!')
-elif player_guess[0] == todays_word[0] :
-    checked_guess[0] = player_guess[0].upper()
-elif player_guess[0] in todays_word :
-    checked_guess[0] = player_guess[0]
-else :
-    checked_guess[0] = 'x'      
+else : 
+    for i in range(5) :
+        if player_guess[i] == todays_word[i] :
+            checked_guess[i] = GREEN + player_guess[i] + RESET
+        elif player_guess[i] in todays_word :
+            checked_guess[i] = YELLOW + player_guess[i] + RESET
+        else :
+            checked_guess[i] = RED + player_guess[i] + RESET
+    checked_guess_string = ''.join(checked_guess)
+    print(checked_guess_string)
 
 
 #if 
